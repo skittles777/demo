0aa694d53e5780f79ce4df2edac8e35c9ebed105
P51 <7135642@qq.com>
2020-08-20 00:36:57 +0800

    add strcmp

---
+++ strcmp.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void main()
+{
+    const char *str1 = "book";
+    const char *str2 = "box";
+    printf("%d", strcmp(str1, str2));
+}
